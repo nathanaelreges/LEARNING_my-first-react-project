02be89a47e9d58e256bf89e6fb30ba5f4537513e
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-07-25 19:26:45 -0300

    React component with props

--- src/component.jsx
+++ src/component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 class Component extends React.Component {
    render () {
-      return <h1>Ola React Component</h1>
+      return <h1> Ola React Component {this.props.val} </h1>
    }
 }
 
